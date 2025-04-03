c7e6838d68b34f8699de2fc359f47c19834436cf
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-03 15:07:52 -0700

    Update from Megan

--- Stack.py
+++ Stack.py
@@ -2,6 +2,37 @@ from Node import Node
 from LinkedList import LinkedList
 
 class Stack:
+  def __init__(self):
+    self.linked_list = LinkedList()
+    self.size = 0
+
+  def push(self, new_item):   # substitute "cancellation_details" for "new_item"
+    new_node = Node(new_item)
+    self.linked_list.prepend(new_node)
+    self.size += 1
+
+  def pop(self):
+    popped_item = self.linked_list.head.data
+    self.linked_list.remove_after(None)
+    self.size -= 1
+    return popped_item
+
+  def peek(self):
+    if self.linked_list.head == None:
+      raise IndexError("Peek from empty stack")
+    return self.linked_list.head.data
+
+  def is_empty(self):
+    return self.linked_list.head == None
+
+  def get_size(self):
+    return self.size
+
+'''
+from Node import Node
+from LinkedList import LinkedList
+
+class Stack:
     def __init__(self):
         self.list = LinkedList()
         
@@ -30,6 +61,6 @@ class Stack:
 
     def is_empty(self):
         pass
-
+'''
     def get_size(self):
         pass
