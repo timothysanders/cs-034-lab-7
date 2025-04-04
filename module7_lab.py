135ce3582ff72c7467062c9dbc7e6ea11f7eb9d4
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-03 19:08:43 -0700

    Update module7_lab.py

--- module7_lab.py
+++ module7_lab.py
@@ -1,4 +1,6 @@
-#Name(s): Michael Jung ID:10680322, Timothy Sanders ID:, Megan Yu ID:
+#Name(s): Michael Jung ID:10680322, Timothy Sanders ID:, Yu Yu Ng ID:
+import unittest
+
 
 class Node:
    def __init__(self, initial_data):
@@ -141,3 +143,38 @@ class Queue:
         # Assign new list and reset front_index to 0
         self.queue_list = new_list
         self.front_index = 0
+
+
+
+class TestTicketSystem(unittest.TestCase):
+
+    def test_stack_LIFO_behavior(self):
+        stack = Stack()
+        stack.push("Cancel ticket #101")
+        stack.push("Cancel ticket #102")
+        stack.push("Cancel ticket #103")
+
+        self.assertEqual(stack.get_size(), 3)
+        self.assertEqual(stack.peek(), "Cancel ticket #103")
+
+        self.assertEqual(stack.pop(), "Cancel ticket #103")
+        self.assertEqual(stack.pop(), "Cancel ticket #102")
+        self.assertEqual(stack.pop(), "Cancel ticket #101")
+        self.assertTrue(stack.is_empty())
+
+    def test_queue_FIFO_behavior(self):
+        queue = Queue()
+        queue.enqueue("Call from customer A")
+        queue.enqueue("Call from customer B")
+        queue.enqueue("Call from customer C")
+
+        self.assertEqual(queue.get_size(), 3)
+        self.assertEqual(queue.front(), "Call from customer A")
+
+        self.assertEqual(queue.dequeue(), "Call from customer A")
+        self.assertEqual(queue.dequeue(), "Call from customer B")
+        self.assertEqual(queue.dequeue(), "Call from customer C")
+        self.assertTrue(queue.is_empty())
+
+
+unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestTicketSystem))
