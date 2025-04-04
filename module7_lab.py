a00fba79d232d298b8d21d1bc51e0cb631317f11
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-03 19:04:55 -0700

    Update module7_lab.py

--- module7_lab.py
+++ module7_lab.py
@@ -1,2 +1,8 @@
 #Name(s): Michael Jung ID:10680322, Timothy Sanders ID:, Megan Yu ID:
 
+class Node:
+   def __init__(self, initial_data):
+      self.data = initial_data
+      self.next = None
+
+
