4f40d2567130aa2f5a53c1a5601bd04e72301384
Timothy Sanders <sanders.timothy.j@gmail.com>
2025-04-03 23:23:55 -0700

    Adding docstrings and type hints; indentation to four spaces

--- LinkedList.py
+++ LinkedList.py
@@ -1,45 +1,120 @@
+import Node
+
 class LinkedList:
-   def __init__(self):
-      self.head = None
-      self.tail = None
+    """
+    Implements a linked list data structure.
 
-   def append(self, new_node):
-      if self.head is None:
-         self.head = new_node
-         self.tail = new_node
-      else:
-         self.tail.next = new_node
-         self.tail = new_node
+    Attributes
+    ----------
+    head : Node
+    tail : Node
 
-   def prepend(self, new_node):
-      if self.head is None:
-         self.head = new_node
-         self.tail = new_node
-      else:
-         new_node.next = self.head
-         self.head = new_node
+    Methods
+    -------
+    append(new_node)
+        Add a new node to the end of the linked list.
+    prepend(new_node)
+        Add a new node to the beginning of the linked list.
+    insert_after(current_node, new_node)
+        Insert a new node after the specified node.
+    remove_after(current_node)
+        Remove the node after the specified node.
+    """
+    def __init__(self):
+        self.head: Node = None
+        self.tail: Node = None
 
-   def insert_after(self, current_node, new_node):
-      if self.head is None:
-         self.head = new_node
-         self.tail = new_node
-      elif current_node is self.tail:
-         self.tail.next = new_node
-         self.tail = new_node
-      else:
-         new_node.next = current_node.next
-         current_node.next = new_node
-   
-   def remove_after(self, current_node):
-     # Special case, remove head
-     if (current_node is None) and (self.head is not None):
-        succeeding_node = self.head.next
-        self.head = succeeding_node  
-        if succeeding_node is None:    # Removed last item
-           self.tail = None
-     elif current_node.next is not None:
-        succeeding_node = current_node.next.next
-        current_node.next = succeeding_node
-        if succeeding_node is None:    # Removed tail
-           self.tail = current_node
+    def append(self, new_node: Node) -> None:
+        """
+        Add a new node to the end of the linked list.
+
+        Parameters
+        ----------
+        new_node : Node
+            The node to add to the end of the linked list.
+
+        Returns
+        -------
+        None
+        """
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def prepend(self, new_node: Node) -> None:
+        """
+        Add a new node to the beginning of the linked list.
+
+        Parameters
+        ----------
+        new_node : Node
+            The node to be added to the beginning of the linked list.
+
+        Returns
+        -------
+        None
+        """
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_after(self, current_node: Node, new_node: Node) -> None:
+        """
+        Insert a new node after the specified node.
+
+        If the list is empty, the new node becomes both the head and tail.
+
+        Parameters
+        ----------
+        current_node : Node
+            The anchor node after which the new node will be inserted.
+        new_node : Node
+            The new node to be inserted.
+
+        Returns
+        -------
+        None
+        """
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif current_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            new_node.next = current_node.next
+            current_node.next = new_node
+
+    def remove_after(self, current_node: Node) -> None:
+        """
+        Remove the node after the specified node.
+
+        Note that passing None as current_node results in removing the head.
+
+        Parameters
+        ----------
+        current_node : Node
+            Node before the node to remove.
+
+        Returns
+        -------
+        None
+        """
+        # Special case, remove head
+        if (current_node is None) and (self.head is not None):
+            succeeding_node = self.head.next
+            self.head = succeeding_node
+            if succeeding_node is None:    # Removed last item
+                self.tail = None
+        elif current_node.next is not None:
+            succeeding_node = current_node.next.next
+            current_node.next = succeeding_node
+            if succeeding_node is None:    # Removed tail
+                self.tail = current_node
 
