61f587030ce41d98843fb511cb1efd601e1baac1
Timothy Sanders <sanders.timothy.j@gmail.com>
2025-04-03 20:10:35 -0700

    Removed previous Stack implementation; indentation to four spaces

--- Stack.py
+++ Stack.py
@@ -4,87 +4,48 @@ import unittest
 
 
 class Stack:
-  def __init__(self):
-    self.linked_list = LinkedList()
-    self.size = 0
+    def __init__(self):
+        self.linked_list = LinkedList()
+        self.size = 0
 
-  def push(self, new_item):   # substitute "cancellation_details" for "new_item"
-    new_node = Node(new_item)
-    self.linked_list.prepend(new_node)
-    self.size += 1
+    def push(self, new_item):   # substitute "cancellation_details" for "new_item"
+        new_node = Node(new_item)
+        self.linked_list.prepend(new_node)
+        self.size += 1
 
-  def pop(self):
-    popped_item = self.linked_list.head.data
-    self.linked_list.remove_after(None)
-    self.size -= 1
-    return popped_item
+    def pop(self):
+        popped_item = self.linked_list.head.data
+        self.linked_list.remove_after(None)
+        self.size -= 1
+        return popped_item
 
-  def peek(self):
-    if self.linked_list.head == None:
-      raise IndexError("Peek from empty stack")
-    return self.linked_list.head.data
+    def peek(self):
+        if self.linked_list.head == None:
+            raise IndexError("Peek from empty stack")
+        return self.linked_list.head.data
 
-  def is_empty(self):
-    return self.linked_list.head == None
+    def is_empty(self):
+        return self.linked_list.head == None
 
-  def get_size(self):
-    return self.size
+    def get_size(self):
+        return self.size
 
 
 class TestLinkedListStack(unittest.TestCase):
 
-  def test_stack_LIFO_behavior(self):
-    stack = Stack()
-    stack.push("Cancel ticket #101")
-    stack.push("Cancel ticket #102")
-    stack.push("Cancel ticket #103")
+    def test_stack_LIFO_behavior(self):
+        stack = Stack()
+        stack.push("Cancel ticket #101")
+        stack.push("Cancel ticket #102")
+        stack.push("Cancel ticket #103")
 
-    self.assertEqual(stack.get_size(), 3)
-    self.assertEqual(stack.peek(), "Cancel ticket #103")
+        self.assertEqual(stack.get_size(), 3)
+        self.assertEqual(stack.peek(), "Cancel ticket #103")
 
-    self.assertEqual(stack.pop(), "Cancel ticket #103")
-    self.assertEqual(stack.pop(), "Cancel ticket #102")
-    self.assertEqual(stack.pop(), "Cancel ticket #101")
-    self.assertTrue(stack.is_empty())
+        self.assertEqual(stack.pop(), "Cancel ticket #103")
+        self.assertEqual(stack.pop(), "Cancel ticket #102")
+        self.assertEqual(stack.pop(), "Cancel ticket #101")
+        self.assertTrue(stack.is_empty())
 
 # Test the functionality of Stack
 unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestLinkedListStack))
-  
-
-
-'''
-from Node import Node
-from LinkedList import LinkedList
-
-class Stack:
-    def __init__(self):
-        self.list = LinkedList()
-        
-    def push(self, new_item):
-        # Create a new node to hold the item
-        new_node = Node(new_item)
-        
-        # Insert the node as the list head (top of stack)
-        self.list.prepend(new_node)
-    
-    def pop(self):
-        # Copy data from list's head node (stack's top node)
-        popped_item = self.list.head.data
-        
-        # Remove list head
-        self.list.remove_after(None)
-        
-        # Return the popped item
-        return popped_item
-
-    def push(self, cancellation_details):
-        pass
-
-    def peek(self):
-        pass
-
-    def is_empty(self):
-        pass
-'''
-    def get_size(self):
-        pass
