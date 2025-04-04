d5634ade110fac59a6f1a8ece8372bdb9df0918f
Timothy Sanders <sanders.timothy.j@gmail.com>
2025-04-03 22:44:37 -0700

    Adding docstring to Node class

--- Node.py
+++ Node.py
@@ -1,4 +1,16 @@
 class Node:
+   """
+   Implement a Node class.
+
+   Parameters
+   ----------
+   initial_data
+
+   Attributes
+   ----------
+   data
+   next
+   """
    def __init__(self, initial_data):
       self.data = initial_data
       self.next = None
